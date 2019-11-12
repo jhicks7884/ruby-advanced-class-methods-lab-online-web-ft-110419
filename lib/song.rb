f88eb3e01555b26d1916497df5a3a60e3e3e8008
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-12 18:26:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ def self.find_or_create_by_name(title)
        self.create_by_name(title)
      end
 
-  
+
      def self.new_from_filename(filename)
        song_array = filename.split(" - ")
        song_array[1] = song_array[1].chomp(".mp3")
