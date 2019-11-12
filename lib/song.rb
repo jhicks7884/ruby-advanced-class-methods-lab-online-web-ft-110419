9b20723c0518de1402c489572a45b0eb3f3c0177
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-12 18:26:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,29 @@ def self.find_or_create_by_name(title)
        self.create_by_name(title)
      end
 
- def self.alphabetical
-    sorted = self.all.sort_by {|song| song.name}
-    sorted
-  end
-end
+     def self.alphabetical
+       sorted = self.all.sort_by {|song| song.name}
+       sorted
+     end
+
+     def self.new_from_filename(filename)
+       song_array = filename.split(" - ")
+       song_array[1] = song_array[1].chomp(".mp3")
+       song = self.new
+       song.name = song_array[1]
+       song.artist_name = song_array[0]
+       song
+     end
+
+     def self.create_from_filename(filename)
+       result = self.new_from_filename(filename)
+       song = self.create
+       song.name = result.name
+       song.artist_name = result.artist_name
+       song
+     end
+
+     def self.destroy_all
+       self.all.clear
+     end
+   end
